560808b57df04e12bd5a36246690fb66cc2b8f58
Sean Park <stpnpark@gmail.com>
2017-05-10 00:23:13 -0700

    exercise_1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,4 +9,5 @@ Store.create(name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, women
 Store.create(name: 'Richmond', annual_revenue: 1_260_000, womens_apparel: true)
 Store.create(name: 'Gastown', annual_revenue: 190_000, mens_apparel: true)
 
+
 puts Store.all.count

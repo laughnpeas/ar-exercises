eaceab4684c06f6c89f25a290a52efff1926c57b
Sean Park <stpnpark@gmail.com>
2017-05-10 06:06:57 -0700

    add to store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,6 +9,6 @@ puts "Exercise 6"
 puts "----------"
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60).save
-@store1.employees.create(first_name: "John", last_name: "Travolta", hourly_rate: 40).save
-@store2.employees.create(first_name: "Matt", last_name: "Damon", hourly_rate: 60).save
-@store2.employees.create(first_name: "Sean", last_name: "Park", hourly_rate: 40).save
+@store2.employees.create(first_name: "John", last_name: "Travolta", hourly_rate: 40).save
+@store4.employees.create(first_name: "Matt", last_name: "Damon", hourly_rate: 60).save
+@store5.employees.create(first_name: "Sean", last_name: "Park", hourly_rate: 40).save

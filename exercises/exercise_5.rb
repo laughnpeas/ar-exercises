7b28b1c5dd067126f33e0d8eff082896aa193ade
Sean Park <stpnpark@gmail.com>
2017-05-10 00:55:55 -0700

    exercise_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,16 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# output the total revenue of the entire stores
+
+total = Store.all.sum(:annual_revenue)
+puts "Total revenue of all stores is #{total}"
+# output the average annual revenue for all stores
+
+num_stores = Store.count
+puts "Average of annual revenue of all stores is #{total/num_stores}"
+
+# output the numer of stores which generating $1M or more in annual revenue
+
+puts Store.where('annual_revenue > ?', 1_000_000).count
+
